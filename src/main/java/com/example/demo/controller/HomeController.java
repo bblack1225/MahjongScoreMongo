2d9e4df5b531b2dc949090c388a77bc2fbc0fddd
bblack1225 <black60137@gmail.com>
2020-12-01 21:17:11 +0800

    add test api

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.example.demo.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,5 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/")
 public class HomeController {
 	
+	@GetMapping
+	public ResponseEntity<String> home(){
+		return ResponseEntity.ok("Hello");
+	}
 	
 }
